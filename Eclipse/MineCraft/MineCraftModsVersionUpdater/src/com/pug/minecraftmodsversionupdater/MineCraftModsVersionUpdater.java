18735bef2ca55f895ef20079bb5cfbeb7af0c0fb
PUG <kjl2875@gmail.com>
2017-07-30 14:22:38 +0900

    MineCraftModsVersionUpdater v1.1
    More accurate get the filename and then display downloadUrl from need
    download mods. (But, much slowly working program)

--- Eclipse/MineCraft/MineCraftModsVersionUpdater/src/com/pug/minecraftmodsversionupdater/MineCraftModsVersionUpdater.java
+++ Eclipse/MineCraft/MineCraftModsVersionUpdater/src/com/pug/minecraftmodsversionupdater/MineCraftModsVersionUpdater.java
@@ -2,9 +2,14 @@ package com.pug.minecraftmodsversionupdater;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
+
+import javax.net.ssl.HttpsURLConnection;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -14,43 +19,39 @@ import org.jsoup.select.Elements;
 public class MineCraftModsVersionUpdater
 {
 	final static String configPropertiesFilePath = "config.properties";
+	static String modsPath;
+	static ArrayList<String> curseforgeProjectUrlList;
 
 	public static void main(String[] args)
 	{
-		System.out.println("MineCraft - Mods Version Updater - v1.0");
+		/*
+		  v1.0: first program.
+		  v1.1: More accurate get the filename and then display downloadUrl from need download mods. (But, much slowly working program)
+		*/
+		System.out.println("MineCraft - Mods Version Updater - v1.1");
 		
-		// set propertys
+		// set Properties
 		
-		Properties prop = new Properties();
+		Properties prop;
+		
 		try
 		{
-			final FileInputStream iStream = new FileInputStream(configPropertiesFilePath);
-			prop.load(iStream);
-			iStream.close();	
+			prop = getProperties();				
 		}
-		catch( Exception e )
+		catch( FileNotFoundException e )
 		{
-			e.printStackTrace();
+			System.err.println("Can not found properties file: " + configPropertiesFilePath);
+			return;
+		}
+		catch( IOException e )
+		{
+			System.err.println("Can not read properties file: " + configPropertiesFilePath);
 			return;
 		}
 		
 		// input = mods path, curseforge project url(ex. autoreglib in "https://minecraft.curseforge.com/projects/autoreglib/files")
 		
-		if( args.length < 2 )
-		{
-			System.out.println("input = mods path, curseforge project url list(ex. autoreglib in \"https://minecraft.curseforge.com/projects/autoreglib/files\")");
-			return;
-		}
-		
-		final String modsPath = args[0];
-		final ArrayList<String> curseforgeProjectUrlList = new ArrayList<String>();
-		
-		int i;
-		
-		for( i=1; i<args.length; i++ )
-		{
-			curseforgeProjectUrlList.add(args[i]);	
-		}
+		parseInputParameters(args);
 		
 		// print mods list from path
 		
@@ -62,15 +63,11 @@ public class MineCraftModsVersionUpdater
 			return;
 		}
 		
-		for( i=0; i<modList.size(); i++ )
-		{
-			System.out.println(modList.get(i));
-		}
-		
-		System.out.println(modList.size() + "mods.");
-		System.out.println();
+		printList(modList);
 		
 		// get curseforge filelist from curseforgeProjectUrlList.
+		
+		int i;
 		
 		ArrayList<String> urlList = new ArrayList<String>();
 		
@@ -81,15 +78,69 @@ public class MineCraftModsVersionUpdater
 			System.out.println((i+1) + ": " + url);
 		}
 		
-		for( i=0; i<urlList.size(); i++ )
+		// Version Check
+		
+		try
 		{
-			String url = urlList.get(i);
-			System.out.println((i+1) + ": " + parse(prop, modList, url));
+			for( i=0; i<urlList.size(); i++ )
+			{
+				String url = urlList.get(i);
+				System.out.println((i+1) + ": " + parse(prop, modList, url, prop.getProperty("MineCraftCurseforgeHost")));
+			}	
+		}
+		catch( IOException e )
+		{
+			e.printStackTrace();
 		}
 		
 	}
 	
-	private static String parse(Properties prop, ArrayList<String> modList, String url)
+	
+	
+	private static void printList(List<String> modList)
+	{
+		for( int i=0; i<modList.size(); i++ )
+		{
+			System.out.println(modList.get(i));
+		}
+		
+		System.out.println(modList.size() + "mods.");
+		System.out.println();
+	}
+
+	private static void parseInputParameters(String[] args)
+	{
+
+		if( args.length < 2 )
+		{
+			System.out.println("input = mods path, curseforge project url list(ex. autoreglib in \"https://minecraft.curseforge.com/projects/autoreglib/files\")");
+			return;
+		}
+		
+		modsPath = args[0];
+		curseforgeProjectUrlList = new ArrayList<String>();
+		
+		int i;
+		
+		for( i=1; i<args.length; i++ )
+		{
+			curseforgeProjectUrlList.add(args[i]);	
+		}
+		
+	}
+
+	private static Properties getProperties() throws IOException
+	{
+		Properties prop = new Properties();
+		
+		final FileInputStream iStream = new FileInputStream(configPropertiesFilePath);
+		prop.load(iStream);
+		iStream.close();
+		
+		return prop;
+	}
+
+	private static String parse(Properties prop, ArrayList<String> modList, String url, String host) throws IOException
 	{ // TODO 아마 적어도 1개이상의 파일이 나오지 않으면 파싱에서 오류가 날 것이다. // TODO 더 많은 페이지에 대해서 지원할수 있으면 지원(1페이지 넘어가도록 동일버전이 나온경우는 없는 것 같고, 다음페이지까지 넘어가야 될 정도의 Release가 없는듯해서 넘어가도 될듯)
 		
 		int i;
@@ -110,43 +161,60 @@ public class MineCraftModsVersionUpdater
 		
 		// TODO 파싱을 properties를 통해 변경 가능하도록 개발
 		
-		String rType = null, rFilename = null;
-		Elements recoads = doc.select(".project-file-list-item");
+		String rType = null, rFilename = null, rDownloadUrl = null;
+		Elements recoads = doc.select(".project-file-list-item"); // 컨텐츠 내용은 어차피 날짜 내림차순으로 정렬되있음.
 		
 		for( i=0; i<recoads.size(); i++ )
 		{
-			String type, filename;
+			String type, downloadUrl, filename;
 			Element e = recoads.get(i);
 			
 			type = e.select(".project-file-release-type > div").attr("class");
 			type = type.substring(0, type.indexOf('-'));
-			filename = e.select(".project-file-name-container a:nth-child(1)").text();
+			downloadUrl = host + e.select(".project-file-download-button a").attr("href");
 			
-			if( type.equals("release") )
+			// begin - get filename
+			
+			HttpsURLConnection conn = (HttpsURLConnection)new URL(host + e.select(".project-file-download-button a").attr("href")).openConnection();
+			conn.connect();
+			conn.getResponseCode(); // 이거라도 안하면 Redirect가 안되서 RedirectURL못가져온다, HTTP 307일때 어떻게 잡아야될지 모르겠음.
+			filename = conn.getURL().toString();
+			filename = filename.substring(filename.lastIndexOf('/')+1);
+			conn.disconnect();
+			
+			// end - get filename
+			
+			if( type.equals("release") ) // 이후 나오는 모든type은 현재보다 오래된 버전이다.
 			{
 				rType = type;
 				rFilename = filename;
-				break;
-			} // 반면, alpha나 beta는 release가 있는지 더 봐야 한다.
-			
-			if( type.equals("beta") )
-			{
-				rType = type;
-				rFilename = filename;
+				rDownloadUrl = downloadUrl;
+				break; // 더이상 이보다 높은 버전은 없다.
 			}
-			else if( type.equals("alpha") )
+			
+			if( type.equals("beta") ) // 이후 나오는 type은 오래된beta이거나 alpha,release일 수 있다.
 			{
-				if( rType == null || !rType.equals("beta") )
+				if( rType == null || rType.equals("aplha") ) // 더 높은버전인지 체크
 				{
 					rType = type;
-					rFilename = filename; // 현재 filename이 사이트에서 나온 값때문에 filename이 아니라 그냥 이름으로 나오는 경우가 있다.
+					rFilename = filename;
+					rDownloadUrl = downloadUrl;
+				}
+			}
+			else if( type.equals("alpha") ) // 이후 나오는 type은 오래된alpha이거나 beta,release일 수 있다.
+			{
+				if( rType == null ) // 더 높은버전인지 체크
+				{
+					rType = type;
+					rFilename = filename;
+					rDownloadUrl = downloadUrl;
 				}
 			}
 		}
 		
 		// search same filename in modList // TODO 검색 알고리즘좀 효율적으로...
 		
-		String prefix = "";
+		String prefix = null;
 		
 		for( i=0; i<modList.size(); i++ )
 		{
@@ -155,8 +223,15 @@ public class MineCraftModsVersionUpdater
 				prefix = "(samefile in mods directory) ";
 			}
 		}
-	
-		return prefix + rType + ": " + rFilename; // TODO null, null이 나오는 경우가 없지 않나?
+		
+		if( prefix != null )
+		{
+			return prefix + rType + ": " + rFilename;
+		}
+		else
+		{
+			return rType + ": " + rFilename + ": " + rDownloadUrl;
+		}
 	}
 
 	private static String getUrl(Properties prop, String projectUrl)
